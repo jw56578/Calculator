08da26f8a539ff23a6cb4f66646eb5498b2e67d0
jw56578 <jw56578@gmail.com>
2015-08-21 16:39:03 -0500

    refactor to not rely on console

---
+++ CalculatorCompiler/CalculatorCompiler/Lexer.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// Class
+//      Lexer
+//
+// Implements
+//      Lexical analyser for 4-function calculator
+//
+// Notes
+//      Simple state machine consuming chars from Console, returning tokens when the
+//      largest one matches (esp. in the case of numbers).
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+public class Lexer
+{
+    int lastchar;
+    UInt32 number;
+    Token token;
+    string textToParse;
+    int index = 0;
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      Lexer (constructor)
+    //
+    // Implements
+    //      Initialises a lexer object, clearing number and setting the first character
+    //      to a space (will be consumed later).
+    //
+    public Lexer(string textToParse)
+    {
+        if (string.IsNullOrEmpty(textToParse)) {
+            throw new ArgumentException("I cant parse nothing", "textToParse");
+        }
+        lastchar = ' ';
+        number = 0;
+        token = Token.NO_TOK;
+        this.textToParse = textToParse;
+        
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      lex
+    //
+    // Implements
+    //      The lexer itself.  Each call consumes characters from Console until a match
+    //      the largest lexical item is found.  For most items this will be a single 
+    //      character, but for numbers we keep consuming chars until either the next
+    //      char is not a number (in which case we stop munching and return a NUMBER
+    //      token), or the number that would be produced is just far too large to fit
+    //      into the integer type we're using for numbers.
+    //      Note: a future implementation might want to handle this second case a bit
+    //      more elegantly than just maxing out.
+    //
+    /*
+     This thing is keeping a reference to the last character (starting it off with " ") and the next character so that you don't have to do a Peek type thing
+     * you always have a reference to the current character you are parsing and the next character
+     lastchar = the thing you are currently dealing with
+     * nextchar = is the character after the current character so that you can use it to determine what to do, like if its a multi digit number
+     * 
+     * 
+     * this structure is avoiding doing things like having a readNumber function to read in characters that form one number
+     * the whole point of the lexer is to tokenize things from one character at a time, when the token may be built from multiple characters like "3222" vs "+"
+     */
+    public Token lex()
+    {
+        int nextchar;
+
+        token = Token.NO_TOK;
+        number = 0;
+        //Once a valid meaning full token is found in full like a number "111" or a operator "+" then the loop will exit and the token will exist for the parser
+        while (token == Token.NO_TOK)
+        {
+            if (this.index == this.textToParse.Length)
+            {
+                token = Token.EOF;
+                return token;
+            }
+            nextchar = this.textToParse[this.index];
+
+            switch (lastchar)
+            {
+                // Catch end of input
+                case -1: token = Token.EOF; break;
+
+                // Operators and other tokens
+                case '+': token = Token.PLUS; break;
+                case '-': token = Token.MINUS; break;
+                case '*': token = Token.TIMES; break;
+                case '/': token = Token.DIVIDE; break;
+                case '(': token = Token.LPAREN; break;
+                case ')': token = Token.RPAREN; break;
+                case ';': token = Token.EOL; break;
+
+                // Numbers.  Check for maxmimum
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    //no idea what this is doing?? Its just a complicated way to check that an number is within range so no stack overflow happens
+                    if (number <= (UInt32.MaxValue / 10))
+                        //what the hell is this going. just a complicated way to concat number values together i guess?
+                        number = number * 10 + (UInt32)(((UInt32)lastchar) - '0');
+                    else
+                        number = UInt32.MaxValue;
+                    //this is checking the next character to determine if its a number, if its not, then the number part is done and you can move to the next step
+                    //if it is a number then you have to keep going in order to get the entire number with all the digits
+                    if (nextchar < '0' || nextchar > '9')
+                        token = Token.NUMBER;
+                    break;
+
+                // Consume everything else
+                default: break;
+            }
+
+            lastchar = nextchar;
+            this.index++;
+        }
+
+        return token;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      getNumber
+    //
+    // Implements
+    //      Accessor method for number.
+    //
+    public UInt32 getNumber() { return number; }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      getToken
+    //
+    // Implements
+    //      Accessor method for token.
+    //
+    public Token getToken() { return token; }
+
+}  // class Lexer ////////////////////////////////////////////////////////////////////////
---
+++ CalculatorCompiler/CalculatorCompiler/Parser.cs
@@ -0,0 +1,188 @@
+﻿
+using System;
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+// Class
+//      Parser
+//
+// Implements
+//      Recursive-descent parser for four function calculator.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+public class Parser
+{
+    Lexer lexer;
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      Parser (constructor)
+    //
+    // Implements
+    //      Saves the lexer object in this parser instance.
+    //
+    public Parser(Lexer l)
+    {
+        lexer = l;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      Parse
+    //
+    // Implements
+    //      Four-function calculator with parentheses.  Additionally, ';' terminates an
+    //      expression, allowing an expression to span multiple lines.
+    //      The grammar is straight from the lecture notes, section 4.1:
+    //
+    //      T -> T + F | T - F | F
+    //      F -> F * P | F / P | P
+    //      P -> ( T ) | n
+    //
+    //      To end a session enter Ctrl-D followed by Enter.
+    //
+    //      The top-level function Parse() make suse of helper functions RdT(), RdF() and
+    //      which implement each of the rules above.
+    //
+    public void Parse()
+    {
+        Int32 result = 0;
+
+        while (true)
+        {
+            lexer.lex();
+
+            switch (lexer.getToken())
+            {
+                case Token.EOF: return;
+
+                case Token.EOL:
+                    Console.WriteLine("= " + result);
+                    break;
+
+                default:
+                    result = DoAddSubtraction();
+                    Console.WriteLine("= " + result);
+                    break;
+            }
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      RdT
+    //
+    // Implements
+    //      The production rule:   T -> T + F | T - F | F
+    //
+    /*
+     this is the first thing that gets run when a token is found because it is for add/subtract you have to keep going further to do the multiple/dividse first
+     * you can see it is calling Rdf which represnts rules that need to be run before Rdt is run
+     */
+    Int32 DoAddSubtraction()
+    {
+        Int32 result = DoMultiplyDivide();
+
+        while (true)
+        {
+            switch (lexer.getToken())
+            {
+                case Token.PLUS:
+                    lexer.lex();
+                    result += DoMultiplyDivide();
+                    continue;
+
+                case Token.MINUS:
+                    lexer.lex();
+                    result -= DoMultiplyDivide();
+                    continue;
+
+                default:
+                    return result;
+            }
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      RdF
+    //
+    // Implements
+    //      The production rule:  F -> F * P | F / P | P
+    //
+    /*
+     
+     */
+    Int32 DoMultiplyDivide()
+    {
+        //call the function with the rules in it that need to be checked before this
+        Int32 result = DoNumberParenthesis();
+
+        while (true)
+        {
+            switch (lexer.getToken())
+            {
+                case Token.TIMES:
+                    lexer.lex();
+                    result *= DoNumberParenthesis();
+                    continue;
+
+                case Token.DIVIDE:
+                    lexer.lex();
+                    result /= DoNumberParenthesis();
+                    continue;
+
+                default:
+                    return result;
+            }
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Method
+    //      RdP
+    //
+    // Implements
+    //      The production rule:  P -> ( T ) | n
+    //
+    /*
+     I don't understand why getting a number token and a parenthesis token are put together
+     */
+    Int32 DoNumberParenthesis()
+    {
+        Int32 result;
+
+        switch (lexer.getToken())
+        {
+            case Token.NUMBER:
+                result = (Int32)lexer.getNumber();
+                break;
+
+            case Token.LPAREN:
+                lexer.lex();
+                result = DoAddSubtraction();
+                if (lexer.getToken() != Token.RPAREN)
+                {
+                    Console.WriteLine("Error: missing closing ')'");
+                    throw new System.Exception();
+                }
+                break;
+
+            default:
+                Console.WriteLine("Error in expression.");
+                throw new System.Exception();
+        }
+        //so this is the bottom of the logic tree, you have a number that you are returning as the result, so why are you calling lex again?
+        lexer.lex();
+        return result;
+    }
+
+} // class Parser ////////////////////////////////////////////////////////////////////////
